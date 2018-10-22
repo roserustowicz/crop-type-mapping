5eede9288b9b4a85c17c017ccb7786e3732100a0
Rose Rustowicz <rose.rustowicz@gmail.com>
2018-10-22 04:56:21 +0000

    add cloud mask creation to script, clean up, define docstrings

--- scripts/cloud_classifier.py
+++ scripts/cloud_classifier.py
@@ -1,25 +1,32 @@
 import os
-import itertools
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
-import rasterio
 import pandas as pd
-import matplotlib.cm as cm
 import sys
-import time
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import scale
-from sklearn.model_selection import GroupShuffleSplit
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
+from sklearn.metrics import confusion_matrix, f1_score
 
+# Set path so that functions can be imported from the utils script
 sys.path.insert(0, '../')
-from utils.utils import create_categorical_df_col, split_with_group, model_fit, plot_confusion_matrix 
-
+from utils.utils import create_categorical_df_col, split_with_group, plot_confusion_matrix 
+from models.sklearn import model_fit 
 
 def main_cloud_classifier(data_dir, data_fname):
+    """
+    Main script to train and evaluate cloud classification methods. 
+
+    Args: 
+      data_dir - (str) the directory where the cloud data is
+      data_fname - (str) the csv filename that contains the cloud data
+                   to be used for training and evaluation of the cloud
+                   classification model
+    Returns: 
+      model - (sklearn model) the model that has been trained for cloud
+              classification. Can be used later using model.predict(X)
+
+      this function also displays the train, val, and test metrics 
+      (accuracy and f1-score) and plots all confusion matrices
+    """
+
     data_path = os.path.join(data_dir, data_fname)
 
     # Read in all labeled pixels into dataframe
@@ -50,6 +57,10 @@ def main_cloud_classifier(data_dir, data_fname):
     val_f1 = f1_score(y_val, val_pred_lbls, average='micro')
     test_f1 = f1_score(y_test, test_pred_lbls, average='micro')
 
+    print('Training metrics: accuracy {}, f1-score {}'.format(tr_score, tr_f1))
+    print('Validation metrics: accuracy {}, f1-score {}'.format(val_score, val_f1))
+    print('Test metrics: accuracy {}, f1-score {}'.format(test_score, test_f1))
+
     # Get the confusion matrices
     train_cm = confusion_matrix(y_train, train_pred_lbls)
     val_cm = confusion_matrix(y_val, val_pred_lbls)
@@ -63,9 +74,63 @@ def main_cloud_classifier(data_dir, data_fname):
         plot_confusion_matrix(cm, classes=class_names,
                       title='{} confusion matrix, without normalization'.format(split_type))
         plt.show()
+    return model
+
+def save_cloud_masks(s2_data_dir, model, verbose):
+    """
+    Uses a trained model to predict and save cloud masks for sentinel-2 data
+    stored in .npy format
+
+    Args: 
+      s2_data_dir - (str) the directory where .npy files associates with 
+                    sentinel-2 imagery is stored. within this directory, a new
+                    directory 'cloud_masks' will be created and will store the 
+                    saved output cloud mask arrays for each .npy file
+      model - (sklearn model) a trained model used for cloud prediction
+      verbose - (boolean) whether to print outputs of the function
+
+    Returns: 
+      this function saves cloud masks as output in the folder
+      's2_data_dir/cloud_masks' as .npy files. Given a npy file as 
+      input that consists of n timestamps, the output .npy file consists
+      of n corresponding cloud masks
+    """
+    data_fnames = [os.path.join(s2_data_dir, fname) for fname in os.listdir(s2_data_dir) if fname.endswith('.npy')]
+    data_fnames.sort()    
+
+    mask_dir = os.path.join(s2_data_dir, 'cloud_masks')
+    if not os.path.exists(mask_dir):
+        os.mkdir(mask_dir)
+
+    for fname in data_fnames:
+        arr = np.load(fname)
+        mask_arr = np.zeros((arr.shape[1], arr.shape[2], arr.shape[3]))
+
+        for timestamp in range(arr.shape[3]):
+            cur_img = arr[:,:,:,timestamp]
+            # put channels last
+            cur_img = np.transpose(cur_img, (1, 2, 0))
+            # flatten into num smaples x num features (10 channels)
+            cur_img = np.reshape(cur_img, [-1, 10]) 
+            # use cloud model to predict the cloud mask
+            cur_pred = model.predict(cur_img)
+            # reshape prediction into image shape
+            cur_pred = np.reshape(cur_pred, (64, 64))
+            # save prediction for this timestamp into cube
+            mask_arr[:, :, timestamp] = cur_pred
+
+        out_fname = os.path.join(mask_dir, fname.split('/')[-1].split('.')[0] + '_mask.npy')
+        if verbose:
+            print("Mask for {} saved to {}".format(fname, out_fname))
+        np.save(out_fname, mask_arr)
 
 if __name__ == '__main__':
     data_dir = '/home/data/clouds/'
     data_fname = 'clean_samples.csv'
+    s2_data_dir = '/home/data/full/train/s2'
+    verbose = 1
 
-    main_cloud_classifier(data_dir, data_fname)
+    # Train cloud classificatio model
+    model = main_cloud_classifier(data_dir, data_fname)
+    # Use model to save out cloud masks
+    save_cloud_masks(s2_data_dir, model, verbose)
