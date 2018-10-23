e0d870ea433f810b9bd11cda38f99655a59f7e5d
Rose Rustowicz <rose.rustowicz@gmail.com>
2018-10-23 02:06:37 +0000

    Add pseudo code for bidirectional CLSTM

--- models/keras_models.py
+++ models/keras_models.py
@@ -1,6 +1,9 @@
 from keras.models import Sequential
-from keras.layers import Activation, BatchNormalization, Flatten, Dropout
+from keras.layers import InputLayer, Activation, BatchNormalization, Flatten, Dropout
 from keras.layers import Dense, Conv1D, MaxPooling1D
+from keras.layers import Dense, Conv2D, MaxPooling2D
+from keras.layers import Bidirectional, TimeDistributed, concatenate
+from keras.backend import reverse
 
 def make_1d_nn_model(num_classes, num_input_feats):
     """ Defines a keras Sequential 1D NN model 
@@ -46,6 +49,35 @@ def make_1d_cnn_model(num_classes, num_input_feats):
     model.add(Flatten())
     model.add(Dense(1000, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
+
+    return model
+
+def make_1d_resnet_model():
+    pass
+
+def make_bidirectional_clstm_model():
+    """
+    """
+    fwd_seq = Input(shape=(X.shape[1:])) # bands, rows, cols, time
+    rev_seq = Input(shape=(X.shape[1:])) # bands, rows, cols, time
+    
+    shared_CLSTM = ConvLSTM2D(filters=256,
+                              kernel_size=3,
+                              padding='same',
+                              activatio='relu'))
+
+    fwd_features = shared_CLSTM(fwd_seq)
+    rev_features = shared_CLSTM(rev_seq)
+
+    concat_feats = concatenate([fwd_features, rev_features], axis=) 
+
+    predictions = Conv2D(filters=num_crops, 
+                         kernel_size=3, 
+                         padding='same', 
+                         activation='softmax')
+
+    model = Model(inputs=[fwd_seq, reverse(fwd_seq, axes=)], 
+                  outputs=predictions)
 
     return model
 
