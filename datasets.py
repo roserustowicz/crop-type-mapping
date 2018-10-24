c4b3f2629013d7c9be68abea028aa2c98fd60f3a
Robin Cheong <robincheong@gmail.com>
2018-10-24 02:39:51 +0000

    Added function shells for preprocess, data loader code written, untested

--- datasets.py
+++ datasets.py
@@ -9,12 +9,15 @@ from random import shuffle
 
 class CropTypeSequence(Sequence):
 
-    def __init__(self, hdf5_filepath, batch_size, shuffle, use_s1, use_s2):
+    def __init__(self, model_name, hdf5_filepath, batch_size, shuffle, use_s1, use_s2):
+        self.model_name = model_name
         self.batch_size = batch_size
         # open hdf5 file
-        self.hdf5_filepath = hdf5_filepath
         self.shuffle = shuffle
-        self.grid_list = #np.array of all grid numbers in this split
+        with h5py.File(hdf5_filepath, 'r') as hdf5_file:
+            self.data = hdf5_file
+
+        self.grid_list = self.data['/masks'].keys()
         self.num_grids = len(self.grid_list)
         self.use_s1 = use_s1
         self.use_s2 = use_s2
@@ -27,26 +30,22 @@ class CropTypeSequence(Sequence):
         batch_X = []
         batch_Y = []
 
-        with h5py.File(self.hdf5_filepath, 'r') as hdf5_file:
-            self.data = hdf5_file
+        # iterate through and append these together
+        for grid_num in grid_nums:
+            if self.use_s1:
+                s1 = self.data[f'/s1/{grid_num}']
+            if self.use_s2:
+                s2 = self.data[f'/s2/{grid_num}']
 
-            # iterate through and append these together
-            for grid_num in grid_nums:
-                if self.use_s1:
-                    s1 = data[f'/s1/{grid_num}']
-                if self.use_s2:
-                    s2 = data[f'/s2/{grid_num}']
-
-                batch_X.append(someConcatentation(s1, s2))
-
-                batch_Y.append(data[f'/masks/{grid_num}'])
+            batch_X.append(preprocess_grid(np.concatenate(s1, s2), model.name)
+            batch_Y.append(self.data[f'/masks/{grid_num}'])
 
         return np.array(batch_X), np.array(batch_Y)
         # get self.batch_size examples and return
 
 
-
     def on_epoch_end(self):
+        """ Randomizes ordering for next epoch. """
         if self.shuffle == True:
             shuffle(self.grid_list)
 
--- preprocess.py
+++ preprocess.py
@@ -6,3 +6,42 @@ Consider this essentially a util library specifically for data manipulation.
 
 """
 
+def retrieve_mask(grid_name):
+    """ Return the mask of the grid specified by grid_name.
+
+    Args:
+        grid_name - (string) string representation of the grid number
+
+    Returns:
+        mask - (npy arr) mask containing labels for each pixel
+    """
+    mask = None
+    return mask
+
+def retrieve_grid(grid_name):
+    """ Retrieves a concatenation of the s1 and s2 values of the grid specified.
+
+    Args:
+        grid_name - (string) string representation of the grid number
+
+    Returns:
+        grid - (npy array) concatenation of the s1 and s2 values of the grid over time
+    """
+    grid = None
+    return grid
+
+def preprocess_grid(grid, model_name):
+    """ Returns a preprocessed version of the grid based on the model.
+
+    Args:
+        grid - (npy array) concatenation of the s1 and s2 values of the grid
+        model_name - (string) type of model (ex: "C-LSTM")
+
+    """
+
+    if model_name == "C-LSTM":
+        return preprocessForCLSTM(grid)
+
+
+
+
