2052cf15b9b615c858e96a432ea18428f23ab451
Rose Rustowicz <rose.rustowicz@gmail.com>
2018-10-26 06:37:02 +0000

    Remove models directory and add to models.py

--- models.py
+++ models.py
@@ -16,6 +16,25 @@ from keras.layers import Dense, Conv2D, MaxPooling2D
 from keras.layers import Bidirectional, TimeDistributed, concatenate
 from keras.backend import reverse
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+def model_fit(classifier, X, y, save=False, random_state=0):
+    if classifier == 'random_forest':
+        model = RandomForestClassifier(random_state=random_state, n_jobs=-1, n_estimators=50)
+    elif classifier == 'logistic_regression':  
+        model = LogisticRegression(random_state=random_state, solver='lbfgs', multi_class='multinomial')
+    else:
+        return print('You must specify a valid model (random_forest, logistic_regression)')
+        
+    model.fit(X, y)
+
+    if save:
+        fname = '_'.join(classifier, time.strftime("%Y%m%d-%H%M%S"), '.pickle')
+        with open(fname, "wb") as f:
+            pickle.dump(model, open(fname, 'wb'))
+    return model 
+
 def make_1d_nn_model(num_classes, num_input_feats):
     """ Defines a keras Sequential 1D NN model 
     
