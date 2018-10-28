9a68ed4dde9807d73b56562f58f9099654c7215b
Robin Cheong <robincheong@gmail.com>
2018-10-28 09:22:43 +0000

    Updates to model training pipeline

--- models.py
+++ models.py
@@ -20,19 +20,19 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
 def make_rf_model(random_state, n_jobs, n_estimators):
-    """ 
-    Defines a sklearn random forest model. See sci-kit learn 
-    documentation of sklearn.ensemble.RandomForestClassifier 
+    """
+    Defines a sklearn random forest model. See sci-kit learn
+    documentation of sklearn.ensemble.RandomForestClassifier
     for more information and other possible parameters
 
-    Args: 
+    Args:
       random_state - (int) random seed
       n_jobs - (int or None) the number of jobs to run in parallel
-                for both fit and predict. None means 1, -1 means 
+                for both fit and predict. None means 1, -1 means
                 using all processors
       n_estimators - (int) number of trees in the forest
 
-    Returns: 
+    Returns:
       model - a sklearn random forest model
     """
     model = RandomForestClassifier(random_state=random_state, n_jobs=n_jobs, n_estimators=n_estimators)
@@ -40,33 +40,33 @@ def make_rf_model(random_state, n_jobs, n_estimators):
 
 def make_logreg_model(random_state=None, solver='lbfgs', multi_class='multinomial'):
     """
-    Defines a skearn logistic regression model. See ski-kit learn 
+    Defines a skearn logistic regression model. See ski-kit learn
     documentation of sklearn.linear_model.LogisticRegression for
     more information or other possible parameters
 
-    Args: 
+    Args:
       random_state - (int) random seed used to shuffle data
       solver - (str) {'newton-cg', 'lbfgs', 'linlinear', 'sag', 'saga'}
-               for multiclass problems, only 'newton-cg', 'sag', 'saga', 
+               for multiclass problems, only 'newton-cg', 'sag', 'saga',
                and 'lbfgs' handle multinomial loss. See docs for more info
-      multiclass - (str) {'ovr', 'multinomial', 'auto'} for 'ovr', a 
-                   binary problem is fit for each label. For 'multinomial', 
+      multiclass - (str) {'ovr', 'multinomial', 'auto'} for 'ovr', a
+                   binary problem is fit for each label. For 'multinomial',
                    the minimized loss is the multinomial loss fit across
-                   the entire probability distribution, even when binary. 
+                   the entire probability distribution, even when binary.
                    See sci-kit learn docs for more information.
 
-    Returns: 
+    Returns:
       model - a sklearn logistic regression model
     """
     model = LogisticRegression(random_state, solver, multi_class)
     return model
 
 def make_1d_nn_model(num_classes, num_input_feats):
-    """ Defines a keras Sequential 1D NN model 
-    
-    Args: 
-      num_classes - (int) number of classes to predict 
-    Returns: 
+    """ Defines a keras Sequential 1D NN model
+
+    Args:
+      num_classes - (int) number of classes to predict
+    Returns:
       loads self.model as the defined model
     """
     model = Sequential()
@@ -81,11 +81,11 @@ def make_1d_nn_model(num_classes, num_input_feats):
     return model
 
 def make_1d_cnn_model(num_classes, num_input_feats):
-    """ Defines a keras Sequential 1D CNN model 
-    
-    Args: 
-      num_classes - (int) number of classes to predict 
-    Returns: 
+    """ Defines a keras Sequential 1D CNN model
+
+    Args:
+      num_classes - (int) number of classes to predict
+    Returns:
       loads self.model as the defined model
     """
     model = Sequential()
@@ -102,11 +102,11 @@ def make_1d_cnn_model(num_classes, num_input_feats):
     model.add(BatchNormalization())
     model.add(MaxPooling1D(pool_size=2, strides=2))
 
-    model.add(Conv1D(64, kernel_size=3, activation='relu', padding='same',))     
+    model.add(Conv1D(64, kernel_size=3, activation='relu', padding='same',))
     model.add(BatchNormalization())
-    model.add(Conv1D(64, kernel_size=3, activation='relu', padding='same',))     
+    model.add(Conv1D(64, kernel_size=3, activation='relu', padding='same',))
     model.add(BatchNormalization())
-    model.add(Conv1D(64, kernel_size=3, activation='relu', padding='same',))     
+    model.add(Conv1D(64, kernel_size=3, activation='relu', padding='same',))
     model.add(BatchNormalization())
     model.add(MaxPooling1D(pool_size=2, strides=2))
 
@@ -129,7 +129,7 @@ def make_bidirectional_clstm_model():
     """
     fwd_seq = Input(shape=(X.shape[1:])) # bands, rows, cols, time
     rev_seq = Input(shape=(X.shape[1:])) # bands, rows, cols, time
-    
+
     shared_CLSTM = ConvLSTM2D(filters=256,
                               kernel_size=3,
                               padding='same',
@@ -138,22 +138,22 @@ def make_bidirectional_clstm_model():
     fwd_features = shared_CLSTM(fwd_seq)
     rev_features = shared_CLSTM(rev_seq)
 
-    concat_feats = concatenate([fwd_features, rev_features], axis=0) # change axis 
+    concat_feats = concatenate([fwd_features, rev_features], axis=0) # change axis
 
-    predictions = Conv2D(filters=num_crops, 
-                         kernel_size=3, 
-                         padding='same', 
+    predictions = Conv2D(filters=num_crops,
+                         kernel_size=3,
+                         padding='same',
                          activation='softmax')
 
-    model = Model(inputs=[fwd_seq, reverse(fwd_seq, axes=0)], # change axes 
+    model = Model(inputs=[fwd_seq, reverse(fwd_seq, axes=0)], # change axes
                   outputs=predictions)
 
     return model
 
 def get_model(model_name, **kwargs):
     if model_name == 'random_forest':
-        model = make_rf_model(random_state=kwargs.get('random_state', None), 
-                                        n_jobs=kwargs.get('n_jobs', -1), 
+        model = make_rf_model(random_state=kwargs.get('random_state', None),
+                                        n_jobs=kwargs.get('n_jobs', -1),
                                         n_estimators=kwargs.get('n_estimators', 50))
 
     return model
--- train.py
+++ train.py
@@ -3,8 +3,11 @@
 Script for training and evaluating a model
 
 """
-
+import os
 import argparse
+import h5py
+from datasets import *
+import models
 
 
 def evaluate(model, inputs, labels, loss_fn):
@@ -23,10 +26,13 @@ def evaluate(model, inputs, labels, loss_fn):
 
     return -1
 
-def train(model, X, y, model_name=None):
+def train(model, model_name, args=None, datagens=None, X=None, y=None):
     """ Trains the model on the inputs"""
     if model_name in ['random_forest', 'logistic_regression']:
         model.fit(X, y)
+    else:
+        assert datagens != None, "DATA GENERATOR IS NONE"
+        history = model.fit_generator(generator=datagens['train'], epochs=args.epochs, validation_data=datagens['val'], workers=8, use_multiprocessing=True, shuffle=args.shuffle)
 
     return model
 
@@ -36,16 +42,54 @@ def get_loss_fn(args):
 if __name__ == "__main__":
     pass
     # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str,
+                        help="model's name")
+    parser.add_argument('--hdf5_filepath', type=str,
+                        help="full path to hdf5 data file",
+                        default="/home/data/ghana/data.hdf5")
+    parser.add_argument('--dataset', type=str,
+                        help="Full or small?",
+                        choices=('full', 'small'))
+    parser.add_argument('--country', type=str,
+                        help="country to predict over",
+                        default="ghana")
+    parser.add_argument('--grid_dir', type=str,
+                        help="full path to directory containing grid splits",
+                        default="/home/data/ghana")
+    parser.add_argument('--epochs', type=int,
+                        help="# of times to train over the dataset",
+                        required=True)
+    parser.add_argument('--batch_size', type=int,
+                        help="batch size to use",
+                        required=True)
+    parser.add_argument('--shuffle', type=bool,
+                        help="shuffle dataset between epochs?",
+                        default=True)
+    parser.add_argument('--use_s1', type=bool,
+                        help="use s1 data?",
+                        default=True)
+    parser.add_argument('--use_s2', type=bool,
+                        help="use s2 data?",
+                        default=True)
+    args = parser.parse_args()
 
     # load in data generator
+    datagens = {}
+    for split in ['train', 'val', 'test']:
+        grid_path = os.path.join(args.grid_dir, f"{args.country}_{args.dataset}_{split}")
+        datagens[split] = CropTypeSequence(args.model_name, args.hdf5_filepath, grid_path, args.batch_size, args.use_s1, args.use_s2)
 
     # load in loss function / optimizer
 
-    # load in model
+    # TODO: make more general to allow for simpler baseline models
 
+    # load in model
+    model_gen_fn = models.__dict__[f'make_{args.model_name}_model']
+    model = model_gen_fn(**vars(args))
     # train model
-    
+    train(model, model_name, args, datagens=datagens)
     # evaluate model
 
     # save model
-    
+
