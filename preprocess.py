Fix to keep rotation amount the same

preprocess.py
@@ -161,7 +161,7 @@ def retrieve_grid(grid_name, country):
     grid = None
     return grid
 
-def preprocess_grid(grid, model_name, time_slice=None, transform=False):
+def preprocess_grid(grid, model_name, time_slice=None, transform=False, rot=None):
     """ Returns a preprocessed version of the grid based on the model.
 
     Args:
@@ -171,14 +171,14 @@ def preprocess_grid(grid, model_name, time_slice=None, transform=False):
     """
 
     if model_name == "bidir_clstm":
-        return preprocessGridForCLSTM(grid, transform)
+        return preprocessGridForCLSTM(grid, transform, rot)
     
     if model_name == "fcn":
         return preprocessGridForFCN(grid, time_slice)
 
     raise ValueError(f'Model: {model_name} unsupported')
 
-def preprocess_label(label, model_name, num_classes=None):
+def preprocess_label(label, model_name, num_classes=None, transform=False, rot=None):
     """ Returns a preprocess version of the label based on the model.
 
     Usually this just means converting to a one hot representation and 
@@ -192,7 +192,7 @@ def preprocess_label(label, model_name, num_classes=None):
     """
     if model_name == "bidir_clstm":
         assert not num_classes is None
-        return preprocessLabelForCLSTM(label, num_classes)
+        return preprocessLabelForCLSTM(label, num_classes, transform, rot)
     
     if model_name == "fcn":
         assert not num_classes is None
@@ -200,7 +200,7 @@ def preprocess_label(label, model_name, num_classes=None):
 
     raise ValueError(f'Model: {model_name} unsupported')
     
-def preprocessLabelForCLSTM(label, num_classes, transform):
+def preprocessLabelForCLSTM(label, num_classes, transform, rot):
     """ Converts to onehot encoding and shifts channels to be first dim.
 
     Args:
@@ -209,7 +209,7 @@ def preprocessLabelForCLSTM(label, num_classes, transform):
     """
     if transform:
         label = np.fliplr(grid)
-        label = np.rot90(grid, k=np.random.randint(0,4))
+        label = np.rot90(grid, k=rot)
 
     label = onehot_mask(label, num_classes)
     label =  np.transpose(label, [2, 0, 1])
@@ -229,11 +229,11 @@ def preprocessLabelForFCN(label, num_classes):
     label = torch.tensor(label, dtype=torch.float32)
     return label
 
-def preprocessGridForCLSTM(grid, transform):
+def preprocessGridForCLSTM(grid, transform, rot):
     grid = moveTimeToStart(grid)
     if transform:
         grid = grid[:, :, :, ::-1]
-        grid = np.rot90(grid, k=np.random.randint(0, 4), axes=(2, 3))
+        grid = np.rot90(grid, k=rot, axes=(2, 3))
     grid = torch.tensor(grid, dtype=torch.float32)
     normalize = transforms.Normalize([0] * grid.shape[1], [1] * grid.shape[1])
     for timestamp in range(grid.shape[0]):