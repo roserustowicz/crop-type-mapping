f6e5ade6a18c2018ce5ef5a9d46234c87fded11c
roserustowicz <rose.rustowicz@gmail.com>
2019-02-15 01:05:19 +0000

    add comments

--- scripts/format_germany_data.py
+++ scripts/format_germany_data.py
@@ -37,12 +37,14 @@ for id_dir in fileids:
     assert len(fnames_20m) > 1
     assert len(fnames_60m) > 1
     
+    # Create timestack of 10m images
     imgs_10m = []
     for fname_10m in fnames_10m:
         with rasterio.open(fname_10m) as src:
             imgs_10m.append(np.expand_dims(src.read(), 3)) 
     imgs_10m = np.concatenate(imgs_10m, axis=3)
     
+    # Create timestack of 20m images
     imgs_20m = []
     for fname_20m in fnames_20m:
         with rasterio.open(fname_20m) as src:
@@ -50,6 +52,7 @@ for id_dir in fileids:
     imgs_20m = np.concatenate(imgs_20m, axis=3)
     imgs_20m = resize(imgs_20m, (imgs_20m.shape[0], imgs_10m.shape[1], imgs_10m.shape[2], imgs_20m.shape[3]), mode='reflect')
 
+    # Create timestack of 60m images
     imgs_60m = []
     for fname_60m in fnames_60m:
         with rasterio.open(fname_60m) as src:
@@ -57,34 +60,33 @@ for id_dir in fileids:
     imgs_60m = np.concatenate(imgs_60m, axis=3)
     imgs_60m = resize(imgs_60m, (imgs_20m.shape[0], imgs_10m.shape[1], imgs_10m.shape[2], imgs_20m.shape[3]), mode='reflect')
     
+    # Reorder and concatenate all bands
     all_bands = [np.expand_dims(imgs_10m[0], 0), np.expand_dims(imgs_10m[1], 0), np.expand_dims(imgs_10m[2], 0),
                  np.expand_dims(imgs_20m[0], 0), np.expand_dims(imgs_20m[1], 0), np.expand_dims(imgs_20m[2], 0),
                  np.expand_dims(imgs_10m[3], 0), np.expand_dims(imgs_20m[3], 0), np.expand_dims(imgs_20m[4], 0),
                  np.expand_dims(imgs_20m[5], 0), np.expand_dims(imgs_60m[0], 0), np.expand_dims(imgs_60m[1], 0), 
                  np.expand_dims(imgs_60m[2], 0)]
-    
     all_bands = np.concatenate(all_bands, axis=0)
-    
+   
+    # Get and convert label using the crop_dict 
     with rasterio.open(fname_label) as src:
         label = np.squeeze(src.read())
-        print(np.unique(label))
- 
     label = convert_label(label, crop_dict)
-    print(np.unique(label))
 
+    # Get dates based on filenames
     dates = ["-".join([f.split("/")[-1].replace("_10m.tif", "")[:4], f.split("/")[-1].replace("_10m.tif", "")[4:6], f.split("/")[-1].replace("_10m.tif", "")[6:]]) for f in fnames_10m]
     meta = {}
     meta["dates"] = dates
 
     cur_fid = id_dir.split('/')[-1]
-    
-    # Save everything
+    # Save label
     output_fname_label = os.path.join(save_dir_lbl, 'germany_'+cur_fid+'_label.npy')
-    output_fname_raster = os.path.join(save_dir_s2, 's2_germany_'+cur_fid+'.npy')
-    output_fname_json = os.path.join(save_dir_s2, 's2_germany_'+cur_fid+'.json')
-    
     np.save(output_fname_label, label)
+    # Save raster stack
+    output_fname_raster = os.path.join(save_dir_s2, 's2_germany_'+cur_fid+'.npy')
     np.save(output_fname_raster, all_bands)
+    # Save json file with dates
+    output_fname_json = os.path.join(save_dir_s2, 's2_germany_'+cur_fid+'.json')
     with open(output_fname_json, 'w') as fp:
         json.dump(meta, fp)  
         
