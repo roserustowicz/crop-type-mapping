12f6a3df1d4ccc42da2350b0fd868fbe85e100ba
roserustowicz <rose.rustowicz@gmail.com>
2019-02-25 21:25:55 +0000

    unet with and without resized planet inputs

--- modelling/unet.py
+++ modelling/unet.py
@@ -20,7 +20,6 @@ class _EncoderBlock(nn.Module):
         ]
         if dropout:
             layers.append(nn.Dropout())
-        #layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
         self.encode = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -59,22 +58,25 @@ class _DecoderBlock(nn.Module):
 class UNet(nn.Module):
     """ U-Net architecture definition
     """
-    def __init__(self, num_classes, num_channels, for_fcn, use_planet, resize_planet):
+    def __init__(self, num_classes, num_channels, for_fcn): #, use_planet, resize_planet):
         super(UNet, self).__init__()
 
-        self.use_planet = use_planet
-        self.resize_planet = resize_planet
+        self.use_planet = True
+        self.resize_planet = False
 
         self.for_fcn = for_fcn
         self.downsample = _DownSample() 
         
         feats = 32
+        if self.use_planet and self.resize_planet:
+            enc3_infeats = num_channels
+        else:
+            enc3_infeats = feats*2
+
         self.enc1 = _EncoderBlock(num_channels, feats)
         self.enc2 = _EncoderBlock(feats, feats*2)
         
-        # WITH PLANET 256 x 256
-        #self.enc3 = _EncoderBlock(64, 128)
-        self.enc3 = _EncoderBlock(num_channels, feats*4)
+        self.enc3 = _EncoderBlock(enc3_infeats, feats*4)
         self.enc4 = _EncoderBlock(feats*4, feats*8)
 
         self.center = nn.Sequential(
@@ -87,89 +89,54 @@ class UNet(nn.Module):
             nn.LeakyReLU(inplace=True),
             nn.ConvTranspose2d(feats*16, feats*8, kernel_size=2, stride=2))    
         self.dec4 = _DecoderBlock(feats*16, feats*8, feats*4)
-        self.dec3 = nn.Sequential(
+        self.final = nn.Sequential(
             nn.Conv2d(feats*8, feats*4, kernel_size=3, padding=1),
             nn.BatchNorm2d(feats*4),
             nn.LeakyReLU(inplace=True),
             nn.Conv2d(feats*4, feats*2, kernel_size=3, padding=1),
             nn.BatchNorm2d(feats*2),
-            nn.LeakyReLU(inplace=True)
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(feats*2, num_classes, kernel_size=3, padding=1),
         )
 
-        self.final = nn.Conv2d(feats*2, num_classes, kernel_size=1)
+        #self.final = nn.Conv2d(feats*2, num_classes, kernel_size=1)
         self.softmax = nn.Softmax2d()
         initialize_weights(self)
 
-        # WITH PLANET RESIZED
-        #self.center = nn.Sequential(
-        #    nn.Conv2d(64, 128, kernel_size=3, padding=1),
-        #    nn.GroupNorm(128 // 16, 128),
-        #    nn.LeakyReLU(inplace=True))
-        #self.center_decode = nn.Sequential(
-        #    nn.Conv2d(128, 128, kernel_size=3, padding=1),
-        #    nn.GroupNorm(128 // 16, 128),
-        #    nn.LeakyReLU(inplace=True),
-        #    nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2))
-        #self.dec2 = _DecoderBlock(128, 64, 32)
-        #self.dec1 = nn.Sequential(
-        #    nn.Conv2d(64, 32, kernel_size=3, padding=1),
-        #    nn.BatchNorm2d(32),
-        #    nn.LeakyReLU(inplace=True),
-        #    nn.Conv2d(32, 32, kernel_size=3, padding=1),
-        #    nn.BatchNorm2d(32),
-        #    nn.LeakyReLU(inplace=True),
-        #)
-        #self.final = nn.Conv2d(32, num_classes, kernel_size=1)
-        #self.softmax = nn.Softmax2d()
-        #initialize_weights(self)
-
     def forward(self, x):
+
+        # ENCODE
         x = x.cuda()
         print('x; ', x.shape)
-        
-        # WITH PLANET 256 x 256
-        #enc1 = self.enc1(x)
-        #down1 = self.downsample(enc1)
-        #print('enc1; ', enc1.shape)
-        #enc2 = self.enc2(down1)
-        #down2 = self.downsample(enc2)
-        #print('enc2; ', enc2.shape)
-        #enc3 = self.enc3(down2)
-        enc3 = self.enc3(x)
+
+        if self.use_planet and self.resize_planet:
+            enc3 = self.enc3(x)
+        else:
+            enc1 = self.enc1(x)
+            down1 = self.downsample(enc1)
+            enc2 = self.enc2(down1)
+            down2 = self.downsample(enc2)
+            enc3 = self.enc3(down2)
+            print('enc2; ', enc2.shape)
+            print('enc1; ', enc1.shape)
+
         down3 = self.downsample(enc3)
-        print('enc3; ', enc3.shape)
         enc4 = self.enc4(down3)
         down4 = self.downsample(enc4)
-        print('enc4; ', enc4.shape)
         center1 = self.center(down4)
-        print('center1; ', center1.shape)
+        
+        # DECODE
         center2 = self.center_decode(center1)
-        print('center2; ', center2.shape)
         dec4 = self.dec4(torch.cat([center2, enc4], 1)) 
+        final = self.final(torch.cat([dec4, enc3], 1)) 
+        #final = self.final(dec3)
+        
+        print('enc3; ', enc3.shape)
+        print('enc4; ', enc4.shape)
+        print('center1; ', center1.shape)
+        print('center2; ', center2.shape)
         print('dec4; ', dec4.shape)
-        dec3 = self.dec3(torch.cat([dec4, enc3], 1)) 
-        print('dec3; ', dec3.shape)
-        final = self.final(dec3)
         print('final: ', final.shape)
-
-
-        # WITH PLANET RESIZED
-        #enc1 = self.enc1(x)
-        #down1 = self.downsample(enc1)
-        #print('enc1; ', enc1.shape)
-        #enc2 = self.enc2(down1)
-        #down2 = self.downsample(enc2)
-        #print('enc2; ', enc2.shape)
-        #center1 = self.center(down2)
-        #print('center1; ', center1.shape)
-        #center2 = self.center_decode(center1)
-        #print('center2; ', center2.shape)
-        #dec2 = self.dec2(torch.cat([center2, enc2], 1)) 
-        #print('dec2; ', dec2.shape)
-        #dec1 = self.dec1(torch.cat([dec2, enc1], 1))
-        #print('dec1; ', dec1.shape)
-        #final = self.final(dec1)
-        #print('final: ', final.shape)
 
         if self.for_fcn:
             return final
