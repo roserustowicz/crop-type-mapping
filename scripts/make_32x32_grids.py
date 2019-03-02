b0c21bb6f455ecd5fa5a8c30d01fdafaaf30337f
robinreversi <robincheong@gmail.com>
2019-03-02 12:18:50 -0800

    Update make_32x32_grids.py
    changes planet data to be 128x128 after resizing

--- scripts/make_32x32_grids.py
+++ scripts/make_32x32_grids.py
@@ -49,6 +49,7 @@ def split_grids(country, num_pixels=32):
     train, val, test = load_splits(country)
     new_splits = {'train': [], 'val': [], 'test': []}
     old_splits = {'train': train, 'val': val, 'test': test}
+    NUM_PLANET_PIXELS = 128
     with h5py.File(HDF5_PATH[country], 'a') as f:
         for split_name, split in old_splits.items():
             for grid in split:
@@ -62,11 +63,17 @@ def split_grids(country, num_pixels=32):
                 s2_dates_grid = f['s2_dates'][grid]
                 cloudmasks_grid = f['cloudmasks'][grid]
                 label = f['labels'][grid]
-
+                
+                planet = None
+                if 'planet' in f.keys():
+                    planet = f['planet'][grid]
+                    planet = imresize(planet, (planet.shape[0], 256, 256, planet.shape[3]), anti_aliasing=True, mode='reflect')
+                
                 s2_sub_grids = []
                 s1_sub_grids = []
                 cloudmasks_sub_grids = []
                 label_sub_grids = []
+                planet_sub_grids = []
 
                 valid = set()
 
@@ -85,6 +92,9 @@ def split_grids(country, num_pixels=32):
                         s2_sub_grids.append(s2_sub_grid)
 
                         cloudmasks_sub_grids.append(cloudmasks_grid[i*num_pixels:(i+1)*num_pixels, j*num_pixels: (j+1)*num_pixels, :])
+                        
+                        planet_sub_grids.append(planet[:, i*NUM_PLANET_PIXELS:(i+1) * NUM_PLANET_PIXELS, j*NUM_PLANET_PIXELS: (j+1) * NUM_PLANET_PIXELS, :])
+                        
                 
                 for i in range(len(label_sub_grids)):
                     sub_grid_name = grid + "_{}".format(i)
@@ -97,7 +107,8 @@ def split_grids(country, num_pixels=32):
                     f.create_dataset("s2_dates/{}".format(sub_grid_name), data=s2_dates_grid, dtype='i2')
                     f.create_dataset("s1/{}".format(sub_grid_name), data=s1_sub_grids[i], dtype='i2', chunks=True)
                     f.create_dataset("s2/{}".format(sub_grid_name), data=s2_sub_grids[i], dtype='i2', chunks=True)
-        
+                    f.create_dataset("planet/{}".format(sub_grid_name), data=planet_sub_grids[i], dtype='i2', chunks=True)
+                    
     save_splits(country, new_splits, old_splits)
     
 if __name__=="__main__":
