58427fec0f79fb4b7f3168396e61e260f95aabb2
roserustowicz <rose.rustowicz@gmail.com>
2019-03-12 08:18:24 +0000

    use nn.LogSoftmax

--- modelling/cgru_segmenter.py
+++ modelling/cgru_segmenter.py
@@ -8,9 +8,10 @@ class CGRUSegmenter(nn.Module):
     """
 
     def __init__(self, input_size, hidden_dims, gru_kernel_sizes, 
-                 conv_kernel_size, gru_num_layers, num_classes, bidirectional):
+                 conv_kernel_size, gru_num_layers, num_classes, bidirectional, early_feats):
 
         super(CGRUSegmenter, self).__init__()
+        self.early_feats = early_feats
 
         if not isinstance(hidden_dims, list):
             hidden_dims = [hidden_dims]        
@@ -20,6 +21,7 @@ class CGRUSegmenter(nn.Module):
         in_channels = hidden_dims[-1] if not self.bidirectional else hidden_dims[-1] * 2
         self.conv = nn.Conv2d(in_channels=in_channels, out_channels=num_classes, kernel_size=conv_kernel_size, padding=int((conv_kernel_size - 1) / 2))
         self.softmax = nn.Softmax2d()
+        self.logsoftmax = nn.LogSoftmax(dim=1)
         initialize_weights(self)
 
     def forward(self, inputs):
@@ -30,7 +32,11 @@ class CGRUSegmenter(nn.Module):
             rev_layer_output_list, rev_last_state_list = self.cgru(rev_inputs)
             final_state = torch.cat([final_state, rev_last_state_list[0][0]], dim=1)
         scores = self.conv(final_state)
-        preds = self.softmax(scores)
-        preds = torch.log(preds)
 
-        return preds
+        if self.early_feats:
+            return scores
+        else:
+            #preds = self.softmax(scores)
+            #preds = torch.log(preds)
+            preds = self.logsoftmax(scores)
+            return preds
--- modelling/clstm_segmenter.py
+++ modelling/clstm_segmenter.py
@@ -65,12 +65,13 @@ class CLSTMSegmenter(nn.Module):
             final_state = torch.cat([final_state, final_state_rev], dim=1)
         scores = self.conv(final_state)
         
-        if not self.early_feats:
+        if self.early_feats:
+            return scores
+        else:
             #preds = self.softmax(scores)
             #print('clstm: ', preds)
             #preds = torch.log(preds)
             preds = self.logsoftmax(scores)
-
-        return preds
+            return preds
 
         
